543484d30a00606947bce5734f8887ac483c746d
Natalie Melkonoff <nmelkonoff@dbg.org>
2021-03-23 10:07:01 -0700

    Initial commit with data simulation and plot

---
+++ simulate-plot-data.R
@@ -0,0 +1,11 @@
+#simulate plot data
+#Natalie Melkonoff
+#nmelkonoff@email.arizona.edu
+#2021-03-23
+
+#simlulate data from normal distribution
+x <- rnorm(n = 100)
+y <- 2 * x + rnorm(n = 100, sd = 0.1)
+
+#plot simulated data
+plot(x = x, y = y)
